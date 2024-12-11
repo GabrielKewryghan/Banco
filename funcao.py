a6e6757939e4fae5126dbd8ab8a800ca1dbcdf07
GabrielKewryghan <g.rosas@aluno.senai.br>
2024-12-11 08:46:44 -0500

    Basico em funções

---
+++ funcao.py
@@ -0,0 +1,14 @@
+#Declarando uma função
+def bom_dia():
+    print("Bom Dia!")
+
+def boa_noite():
+    print("Boa Noite!")
+
+def boa_tarde():
+    print("Boa Tarde!")
+
+#Chamando as funções
+bom_dia()
+boa_noite()
+boa_tarde()
